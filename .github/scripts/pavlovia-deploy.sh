026693708d7c4993237c19f0a619c15f20b75c05
Adam Richie-Halford <richiehalford@gmail.com>
2022-03-28 11:31:06 -0700

    Merge changes in the github ci

--- .github/scripts/pavlovia-deploy.sh
+++ .github/scripts/pavlovia-deploy.sh
@@ -22,9 +22,10 @@ echo -e "\033[0;32mDeploying $branch branch...\033[0m"
 cd $directory &&
   git config user.email "richiehalford@gmail.com" &&
   git config user.name "GitHub Action on behalf of Adam Richie-Halford" &&
+  git remote add $remote $url &&
   git add --all &&
   git commit -m "Deploy updates" &&
-  git remote add $remote $url &&
+  git pull $remote main -Xours --commit &&
   git push $remote $branch:main
 
 echo -e "\033[0;32mCleaning up...\033[0m"
